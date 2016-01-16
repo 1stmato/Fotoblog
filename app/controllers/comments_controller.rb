20fa97946f8c03b1b35e2063c5ebf44b5ddd0aaa
miskoznp <michal.masir@gmail.com>
2016-01-17 00:46:32 +0100

    funny bug introduced

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,36 @@
 class CommentsController < ApplicationController
 
+  def create
+    @post = Post.find(params[:post_id])
+    #render plain: comment_params[:body].inspect
+    setup_comment.save
+    redirect_to post_path(@post)
+end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path(@post)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  private
+  def setup_comment()
+    comment = Comment.new
+    #comment.comment = null
+    comment.user = current_user
+    @post.allow_comments == "Always" ? comment.display = true : comment.display = false
+    comment.validated = false
+    comment.likes = 0
+    comment.dislikes = 0
+    comment.body = comment_params[:body]
+    comment.post = @post
+    return comment
+  end
+
 end
